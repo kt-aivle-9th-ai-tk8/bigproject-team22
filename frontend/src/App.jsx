3c992b3fea898587e3fe0c67c27712d790ab7f25
lsh4323 <xyb456@naver.com>
2026-07-10 16:13:01 +0900

    Screen 분리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import MainScreen from "./screens/MainScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+
 import "./App.css";
 
 function App() {
-  return <MainScreen />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/signup" element={<SignupScreen />} />
+        <Route path="/main" element={<MainScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
