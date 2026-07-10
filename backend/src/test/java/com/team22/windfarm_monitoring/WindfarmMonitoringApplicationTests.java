4daff4a906adc7b462a12328b0eaa258cbbcc4b8
lsh4323 <xyb456@naver.com>
2026-07-10 10:48:53 +0900

    backend baseline

---
+++ backend/src/test/java/com/team22/windfarm_monitoring/WindfarmMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team22.windfarm_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindfarmMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
