cf8dd2e5142a775b99b3109b250a58cf69f90260
IT-Gentleman <gentleman@kakao.com>
2026-07-13 13:50:17 +0900

    프로젝트 구조 및 설정 변경: .gitignore 추가, build.gradle 수정, 애플리케이션 이름 변경

--- backend/src/test/java/kr/co/kt/aivle/_th/ai/team22/windfarmonm/WindfarmonmApplicationTests.java
+++ backend/src/test/java/kr/co/kt/aivle/_th/ai/team22/windfarmonm/WindfarmonmApplicationTests.java
@@ -1,10 +1,10 @@
-package com.team22.windfarm_monitoring;
+package kr.co.kt.aivle._th.ai.team22.windfarmonm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WindfarmMonitoringApplicationTests {
+class WindfarmonmApplicationTests {
 
 	@Test
 	void contextLoads() {
