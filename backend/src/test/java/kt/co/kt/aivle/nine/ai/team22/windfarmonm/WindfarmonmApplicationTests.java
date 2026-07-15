59354109bec9c14fd39f29b977dbdd7cfa54bba2
IT-Gentleman <gentleman@kakao.com>
2026-07-15 10:50:34 +0900

    프로젝트 패키지명 수정: `_th` → `nine`으로 변경, group 값 업데이트

--- backend/src/test/java/kt/co/kt/aivle/nine/ai/team22/windfarmonm/WindfarmonmApplicationTests.java
+++ backend/src/test/java/kt/co/kt/aivle/nine/ai/team22/windfarmonm/WindfarmonmApplicationTests.java
@@ -1,4 +1,4 @@
-package kr.co.kt.aivle._th.ai.team22.windfarmonm;
+package kt.co.kt.aivle.nine.ai.team22.windfarmonm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
