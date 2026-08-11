712d892ba96fe3194ba02acf7cae1b599e867e08
Minwoo Kim <158142406+IT-Gentleman@users.noreply.github.com>
2026-08-11 10:22:59 +0900

    backend cicd test

--- backend/src/main/java/kr/co/kt/aivle/nine/ai/team22/windfarmonm/WindfarmonmApplication.java
+++ backend/src/main/java/kr/co/kt/aivle/nine/ai/team22/windfarmonm/WindfarmonmApplication.java
@@ -3,10 +3,12 @@ package kr.co.kt.aivle.nine.ai.team22.windfarmonm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main Application
 @SpringBootApplication
 public class WindfarmonmApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting...");
 		SpringApplication.run(WindfarmonmApplication.class, args);
 	}
 
